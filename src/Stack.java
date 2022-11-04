874dbf74a40894fa85de02aa27efa1644bec26d6
lillyphan <lphan24@kentdenver.org>
2022-11-04 12:25:00 -0600

    Add instance data, empty constructor, write boolean isEmpty() and int size()

--- src/Stack.java
+++ src/Stack.java
@@ -1,2 +1,15 @@
-public class Stack {
+public class Stack<E>{
+    StackNode<E> top = null;
+    int size = 0;
+
+    public void Stack(){
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return (top == null);
+    }
 }
